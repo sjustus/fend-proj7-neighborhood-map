de964bed8b8f81d1dc93e3aadd623130b48472ad
Shayna Justus <shayna.justus@gmail.com>
2019-01-18 11:02:31 -0800

    Initialize state for locations array

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import Header from './Header.js'
 import Listview from './Listview.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //store locations in state in order to be accessible to Map & Listview components
+    this.state = {
+      locations: [
+        {title: 'Things From Another World', location: {lat: 45.4434415, lng: -122.641392}},
+        {title: 'Dark Horse Comics', location: {lat: 45.44397559999999, lng: -122.640613}},
+        {title: 'Spring Creek Coffee House', location: {lat: 	45.4458543, lng: -122.6424054}},
+        {title: 'Wind Horse Cafe', location: {lat: 45.4460054, lng: -122.6419976}},
+        {title: 'Rohst Coffee', location: {lat: 45.4417007, lng: -122.6393937}}
+      ]
+    }
+  }
   render() {
     return (
       <div id="App">
@@ -12,7 +25,7 @@ class App extends Component {
 
         <main>
           <Listview />
-          <Map />
+          <Map locations={this.state.locations}/>
         </main>
       </div>
     )
--- src/Map.js
+++ src/Map.js
@@ -29,23 +29,14 @@ class Map extends Component {
         zoom: 15
       })
 
-    // create an array to store default locations to be marked when map loads
-    const locations = [
-      {title: 'Things From Another World', location: {lat: 45.4434415, lng: -122.641392}},
-      {title: 'Dark Horse Comics', location: {lat: 45.44397559999999, lng: -122.640613}},
-      {title: 'Spring Creek Coffee House', location: {lat: 	45.4458543, lng: -122.6424054}},
-      {title: 'Wind Horse Cafe', location: {lat: 45.4460054, lng: -122.6419976}},
-      {title: 'Rohst Coffee', location: {lat: 45.4417007, lng: -122.6393937}}
-    ]
-
     // create an array to hold all markers
     const markers = []
 
     // for every location in the locatons array
-    for (let i = 0; i < locations.length; i++) {
+    for (let i = 0; i < this.props.locations.length; i++) {
       // set position and title
-      const position = locations[i].location;
-      const title = locations[i].title;
+      const position = this.props.locations[i].location;
+      const title = this.props.locations[i].title;
       // create a new marker
       const marker = new google.maps.Marker({
         map: map,
